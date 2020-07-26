1711d3a8e4a8542c391fcaf3234b3bcbcd3341f2
Adam Palmer <theadampalmer@gmail.com>
2020-07-26 22:23:54 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -14,14 +14,10 @@ class String
     self.end_with?("!")
   end
 
-  # def count_sentences
-  #   sentences = self.split(/[.?!]/)
-  #   sentences.delete_if{|sentence|sentence.empty?}
-  #   sentences.length
-  # end
-  
-    def count_sentences
-    array = self.split(/[\.!?] /)
-    array.length
+  def count_sentences
+    sentences = self.split(/[.?!]/)
+    sentences.delete_if{|sentence|sentence.empty?}
+    sentences.length
   end
+  
 end
